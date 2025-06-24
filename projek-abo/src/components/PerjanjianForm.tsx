ae3f360ff314ffc998ee0c92e2adca4e7aaaaefc
RafiNizamudin <rafi.nizamudin01@gmail.com>
2025-06-24 16:31:45 +0700

    u

--- projek-abo/src/components/PerjanjianForm.tsx
+++ projek-abo/src/components/PerjanjianForm.tsx
@@ -150,6 +150,18 @@ export default function PerjanjianForm() {
           />
         </div>
 
+        <div>
+<label>
+  tangal lahir
+</label>
+<input type="date" 
+value={tanggal}
+onChange={(e) => setTanggal(e.target.value)}
+            required
+/>
+        </div>
+        
+
         <button type="submit">Buat Perjanjian</button>
       </form>
     </div>
